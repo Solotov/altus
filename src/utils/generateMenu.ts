e91cae6804a64881d7f62187fd893a09693801b7
Aman Harwara <amanharwara@gmail.com>
2020-04-07 14:32:50 +0530

    Update generateMenu.ts

--- src/utils/generateMenu.ts
+++ src/utils/generateMenu.ts
@@ -1,43 +1,40 @@
-import {
-  MenuItemConstructorOptions,
-  BrowserWindow,
-  app,
-  ipcMain
-} from "electron";
+import { BrowserWindow, app } from "electron";
 
-const mainMenu: Array<MenuItemConstructorOptions> = [
+const mainMenu = [
   {
     label: "File",
     submenu: [
-      ...[
-        !app.isPackaged
-          ? {
-              label: "Open DevTools",
-              accelerator: "CmdOrCtrl+Shift+I",
-              click(): void {
-                const window = BrowserWindow.getFocusedWindow();
-                window.webContents.openDevTools();
-              }
-            }
-          : {}
-      ],
       {
         label: "Force Reload",
         accelerator: "CmdOrCtrl+Shift+R",
         click(): void {
           const window = BrowserWindow.getFocusedWindow();
           window.webContents.reload();
-        }
+        },
       },
       {
         label: "Quit",
         accelerator: "CmdOrCtrl+Q",
         click(): void {
           app.quit();
-        }
-      }
-    ]
-  }
+        },
+      },
+    ],
+  },
 ];
+
+if (app.isPackaged) {
+  mainMenu[0].submenu = [
+    {
+      label: "Open DevTools",
+      accelerator: "CmdOrCtrl+Shift+I",
+      click(): void {
+        const window = BrowserWindow.getFocusedWindow();
+        window.webContents.openDevTools();
+      },
+    },
+    ...mainMenu[0].submenu,
+  ];
+}
 
 export default mainMenu;
